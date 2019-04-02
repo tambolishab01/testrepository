c2863fcee56e5f96054c55c870069e7ba71f56da
tambolishab01 <47974840+tambolishab01@users.noreply.github.com>
2019-04-02 17:45:35 +0530

    Update linuxvm.tf

--- linuxvm.tf
+++ linuxvm.tf
@@ -2,6 +2,7 @@
 # Configure the Microsoft Azure Provider.
 provider "azurerm" {
    subscription_id = "939e27d9-0158-4664-94a7-e12525d451c1"
+   client_id       = "508730b9-e6c1-4d18-a80d-e2bd373f5d70"
    tenant_id       = "c160a942-c869-429f-8a96-f8c8296d57db" 
 }
 
