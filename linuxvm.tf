58161fe7995faa622ce705655c779974ea44b9ee
tambolishab01 <47974840+tambolishab01@users.noreply.github.com>
2019-04-02 17:27:43 +0530

    RGcommit

--- linuxvm.tf
+++ linuxvm.tf
@@ -1,8 +1,8 @@
 
 # Configure the Microsoft Azure Provider.
 provider "azurerm" {
-    version = "=1.20.0"
-    subscription_id = "939e27d9-0158-4664-94a7-e12525d451c1"
+   subscription_id = "939e27d9-0158-4664-94a7-e12525d451c1"
+   tenant_id       = "c160a942-c869-429f-8a96-f8c8296d57db" 
 }
 
 # Create a resource group
