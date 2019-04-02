4f7838998cda23af6675a5b92dbb06c72f3d9991
tambolishab01 <47974840+tambolishab01@users.noreply.github.com>
2019-04-02 17:21:40 +0530

    Update linuxvm.tf

--- linuxvm.tf
+++ linuxvm.tf
@@ -2,6 +2,7 @@
 # Configure the Microsoft Azure Provider.
 provider "azurerm" {
     version = "=1.20.0"
+    subscription_id = "939e27d9-0158-4664-94a7-e12525d451c1"
 }
 
 # Create a resource group
